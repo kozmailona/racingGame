9f38391cfef42e510f8ccaab56c2381bbf972254
kozmailona <kozmailona12@gmail.com>
2020-02-24 20:49:00 +0200

    studied classes variables, primitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,41 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the racing game!" );
+
+        Car carReference = new Car();                   //un constructor al clasei
+        carReference.name = "Audi";                     //ctrl+shift+Enter -> beteszi a pontosvesszot, ctrl+click pe name,  vissza a mappaba, ahonnan hivtam
+        carReference.color = "red";
+        carReference.maxSpeed = 260;                    //ctrl+click pe maxSpeed -> informatii despre el
+        carReference.mileage = 6.2;                     // ca separator folosim '.'
+        //ctrl+alt+L -> reformat code
+
+        //concatenation - tobb string osszeragasztasa; lipire de stringuri
+        System.out.println("Properties of " + carReference.name);
+        System.out.println("Color: " + carReference.color);     //ha csak color-t irok? -> meg kell hivnom; variable scope; durata ei de viata
+        System.out.println("Max speed of the car: " + carReference.maxSpeed);
+        System.out.println("Mile age of the car: " + carReference.mileage);
+                //duplicate line??
+        System.out.println("Fuel level: " + carReference.fuelLevel);
+        System.out.println("Racing number: " + carReference.racingNumber);
+        System.out.println("Damaged: " + carReference.damaged);
+
+        //carReference = null; -> am golit
+
+        Car car2 = new Car();
+        car2.name = "Lambourghini";
+        car2.damaged = true;
+
+        System.out.println();
+        System.out.println("Properties of " + car2.name);
+        System.out.println("Name: " + car2.damaged);
+        System.out.println("Color: " + car2.color);     //ctrl+f+f5 -> rerun
+        //exit code = 0 -> ok!
+        //exit code = 1 -> nem ok! :))
+        //null pointer exception
+
+        //ctrl+K -> commit
+
     }
+    //itt mar nem hivhatom meg a carReference-szel
 }
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,20 @@
+package org.fasttrackit;
+
+public class Car {
+
+    /*instance variables; variabile de instanta -> obiectul respectiv, concret, real
+    valorile lor va fi independente
+    */
+
+
+    int racingNumber;
+    String name;
+    int maxSpeed;
+    double mileage;
+    String color;
+    double fuelLevel;
+    boolean damaged;
+
+
+
+}
