7ed9d54c81f7485ad0ccc3fa3dc2185af3063aab
kozmailona <kozmailona12@gmail.com>
2020-05-08 18:04:55 +0300

    studied arrays

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,97 +10,99 @@ public class App {
 
     public static void main( String[] args ) {
 
-//        Game game = new Game();
-//        game.start();
-//
-        new AutoVehicle(new Engine());
+        Game game = new Game();
+        game.start();
 
-        //method implementation taken from object's class, not from variable's class
-        Vehicle cheater = new CheatingVehicle();
-        cheater.setName("Cheater");
-
-        cheater.accelerate(60,1);
-        System.out.println("Total distance: " + cheater.getTotalDistance());
-
-        ((CheatingVehicle)cheater).fly();
-
-
-        //variable type decided what methods can be invoked
-        //type casting
-
-        ( (CheatingVehicle) cheater).fly();
-
-          Vehicle vehicle1 = new Vehicle();
-//        vehicle1.totalVehicleCount = 1;
-
-        Vehicle vehicle2 = new Vehicle();
-//        vehicle2.totalVehicleCount = 2;
-
-//        Vehicle.totalVehicleCount = 50;
-
-        System.out.println("Total count from vehicle 1: " + vehicle1.getTotalVehicleCount());
-        System.out.println("Total count from vehicle 1: " + vehicle2.getTotalVehicleCount());
-        System.out.println("Total count from Vehicle 1: " + Vehicle.getTotalVehicleCount());
-
-
-//        System.out.println("Please enter vehicle name: ");
-//        Scanner scanner = new Scanner(System.in);
-//        String userInput = scanner.nextLine();
-//        System.out.println("Entered name: " + userInput);
-
-        Scanner scanner2 = new Scanner(System.in);
-        int enteredNumber = scanner2.nextInt();
-
-       System.out.println( "Welcome to the racing game!" );
-
-       Car car = new Car();
-
-        Engine engine1 = new Engine();
-        engine1.manufacturer = "Volkswagen";
-        engine1.capacity = 2000;
-
-        Car carReference = new Car(engine1);                                                                        //un
-        // constructor al clasei
-        carReference.setName("Audi");                                                                         //ctrl+shift+Enter -> beteszi a pontosvesszot, ctrl+click pe name,  vissza a mappaba, ahonnan hivtam
-        carReference.setColor("red");
-        carReference.setMaxSpeed(260);                                                                        //ctrl+click pe maxSpeed -> informatii despre el
-        carReference.setMileage(6.2);                                                                         // ca separator folosim '.'
-        carReference.setFuelLevel(100);
-        carReference.setDamaged(true);
-
-        System.out.println("Name: " + carReference.getName());
-        System.out.println(carReference);
-
-        //        carReference.engine = new Engine();
-        //        carReference.engine.manufacturer = "Volkswagen";            //ctrl+D duplicate line
-        //        carReference.engine.capacity = 2000;        //ctrl + /
-
-
-        double currentDistance = carReference.accelerate(60, 1);
-        System.out.println("Current distance: " + currentDistance);
-                                                                                                            //ctrl+alt+L -> reformat code
 
 //
+//        new AutoVehicle(new Engine());
 //
-        System.out.println();
-        Mechanic mechanic = new Mechanic();
-        mechanic.repair(carReference);
-        System.out.println("Is car damaged? " + carReference.isDamaged());
-
+//        //method implementation taken from object's class, not from variable's class
+//        Vehicle cheater = new CheatingVehicle();
+//        cheater.setName("Cheater");
 //
-//        //One object with two references
+//        cheater.accelerate(60,1);
+//        System.out.println("Total distance: " + cheater.getTotalDistance());
 //
-        System.out.println("Engine details: ");
-        System.out.println(engine1.capacity);
-        System.out.println(carReference.getEngine().capacity);
-        System.out.println();
-
-        engine1.capacity = 2100;
-
-        System.out.println("Updated engine details: ");
-        System.out.println(engine1.capacity);
-        System.out.println(carReference.getEngine().capacity);
-        System.out.println();
+//        ((CheatingVehicle)cheater).fly();
+//
+//
+//        //variable type decided what methods can be invoked
+//        //type casting
+//
+//        ( (CheatingVehicle) cheater).fly();
+//
+//          Vehicle vehicle1 = new Vehicle();
+////        vehicle1.totalVehicleCount = 1;
+//
+//        Vehicle vehicle2 = new Vehicle();
+////        vehicle2.totalVehicleCount = 2;
+//
+////        Vehicle.totalVehicleCount = 50;
+//
+//        System.out.println("Total count from vehicle 1: " + vehicle1.getTotalVehicleCount());
+//        System.out.println("Total count from vehicle 1: " + vehicle2.getTotalVehicleCount());
+//        System.out.println("Total count from Vehicle 1: " + Vehicle.getTotalVehicleCount());
+//
+//
+////        System.out.println("Please enter vehicle name: ");
+////        Scanner scanner = new Scanner(System.in);
+////        String userInput = scanner.nextLine();
+////        System.out.println("Entered name: " + userInput);
+//
+//        Scanner scanner2 = new Scanner(System.in);
+//        int enteredNumber = scanner2.nextInt();
+//
+//       System.out.println( "Welcome to the racing game!" );
+//
+//       Car car = new Car();
+//
+//        Engine engine1 = new Engine();
+//        engine1.manufacturer = "Volkswagen";
+//        engine1.capacity = 2000;
+//
+//        Car carReference = new Car(engine1);                                                                        //un
+//        // constructor al clasei
+//        carReference.setName("Audi");                                                                         //ctrl+shift+Enter -> beteszi a pontosvesszot, ctrl+click pe name,  vissza a mappaba, ahonnan hivtam
+//        carReference.setColor("red");
+//        carReference.setMaxSpeed(260);                                                                        //ctrl+click pe maxSpeed -> informatii despre el
+//        carReference.setMileage(6.2);                                                                         // ca separator folosim '.'
+//        carReference.setFuelLevel(100);
+//        carReference.setDamaged(true);
+//
+//        System.out.println("Name: " + carReference.getName());
+//        System.out.println(carReference);
+//
+//        //        carReference.engine = new Engine();
+//        //        carReference.engine.manufacturer = "Volkswagen";            //ctrl+D duplicate line
+//        //        carReference.engine.capacity = 2000;        //ctrl + /
+//
+//
+//        double currentDistance = carReference.accelerate(60, 1);
+//        System.out.println("Current distance: " + currentDistance);
+//                                                                                                            //ctrl+alt+L -> reformat code
+//
+////
+////
+//        System.out.println();
+//        Mechanic mechanic = new Mechanic();
+//        mechanic.repair(carReference);
+//        System.out.println("Is car damaged? " + carReference.isDamaged());
+//
+////
+////        //One object with two references
+////
+//        System.out.println("Engine details: ");
+//        System.out.println(engine1.capacity);
+//        System.out.println(carReference.getEngine().capacity);
+//        System.out.println();
+//
+//        engine1.capacity = 2100;
+//
+//        System.out.println("Updated engine details: ");
+//        System.out.println(engine1.capacity);
+//        System.out.println(carReference.getEngine().capacity);
+//        System.out.println();
 //
 
 //        //concatenation - tobb string osszeragasztasa; lipire de stringuri
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -2,16 +2,57 @@ package org.fasttrackit;
 
 import java.util.Scanner;
 
-public class Game {
+public class  Game {
+
+    private Track[] tracks = new Track[3];
+
+
+    private Vehicle competitor1;
+    private Vehicle competitor2;
 
     public void start() {
         System.out.println("Welcome!");
 
         int playerCount = getPlayerCountFromUser();
 
+        initializeTracks();
+        displayTracks();
+    }
 
+    private void initializeTracks() {
+        Track track1 = new Track();
+        track1.setName("Imola");
+        track1.setLength(3500);
+
+        tracks[0] = track1;
+
+        Track track2 = new Track();
+        track2.setName("Rabbit");
+        track2.setLength(4000);
+
+        tracks[1] = track2;
+
+        Track track3 = new Track();
+        track3.setName("Babi");
+        track3.setLength(4500);
+
+        tracks[2] = track3;
 
     }
+
+    public void displayTracks() {
+        System.out.println("Available tracks: ");
+
+        //classic for loop
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] != null) {
+                System.out.println((i + 1) + ". " + tracks[i].getName() + " - " + tracks[i].getLength());
+                //System.out.println(new Vehicle());
+            }
+        }
+    }
+
+
     private String getVehicleNameFromUser() {                                           //csak ebben a classban van
                                                                                         // hozzaferesem
 
